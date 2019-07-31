612ad3508ace2ac949f2338e890aac1c38afc573
ATufekovic <anto.tufekovic@gmail.com>
2019-07-31 17:53:52 +0200

    finished movement and added checks for operations

--- main/main.ino
+++ main/main.ino
@@ -15,9 +15,14 @@
 AccelStepper xStepper(motorIntefaceType, pinStepPulseX, pinDirectionX);
 AccelStepper yStepper(motorIntefaceType, pinStepPulseY, pinDirectionY);
 
+byte isXDone = true;//flags to let computer know if it's done
+byte isYDone = true;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Starting Serial Monitor");
+
+
 
   pinMode(pinStepperEnable, OUTPUT);//by default is pulled high to disable motors
   digitalWrite(pinStepperEnable, LOW);//pulling low means they will now respond
@@ -29,7 +34,7 @@ void setup() {
 
   xStepper.setMaxSpeed(1000);//sets the maximum allowed speed for the steppers
   yStepper.setMaxSpeed(1000);//speed is measured in steps per second
-  xStepper.setSpeed(600);//ie. microstepping can bring a full revolution up to 6400 steps
+  xStepper.setSpeed(600);//ie. 32x microstepping can bring a full revolution up to 6400 steps
   yStepper.setSpeed(600);//so plan the max speed accordingly to your motor's datasheet
 }
 
@@ -42,14 +47,12 @@ void loop() {
 
     //It will expect a command formatted like "1:+90&2:-80"
     /*
-       commandID:commandValue&commandID:commandValue - can be chained
+       commandID:commandValueOne&commandID:commandValueOne - can be chained
 
        1 - Xmove - set X steps to move with constant speed
        2 - Ymove - set Y steps to move with constant speed
-       3 - Xspeed - set X speed //TODO - probably not needed
-       4 - Yspeed - set Y speed
-       5 - Xacceleration - set X acceleration //TODO - likewise
-       6 - Yacceleration - set Y acceleration
+
+       5 - check if the current operation is done
 
        11 - set current X position to value
        12 - set current Y position to value
@@ -57,8 +60,8 @@ void loop() {
        14 - get current Y position
        15 - stop X
        16 - stop Y
-       17 - disable X
-       18 - disable Y
+       17 - disable X WIP
+       18 - disable Y WIP
     */
 
     char* command = strtok(input, "&");
@@ -70,48 +73,43 @@ void loop() {
         *separator = 0;
         int commandID = atoi(command);
         ++separator;
-        int commandValue = atoi(separator);
 
-        Serial.print("ID= "); Serial.println(commandID);
-        Serial.print("Value= "); Serial.println(commandValue);
-
+        //second value extraction
+        char* separator_two = strchr(separator, ':');
+        ++separator_two;
+        int commandValueTwo = atoi(separator_two);
+        int commandValueOne = atoi(separator);
 
         switch (commandID) {
           case 1 :
-            xStepper.move(commandValue);
-            xStepper.setSpeed(500);
+            xStepper.move(commandValueOne);
+            xStepper.setSpeed(commandValueTwo);
             xStepper.runSpeedToPosition();
-            Serial.println("Move X");//TODO: remove every one of these on completion
+            Serial.print("Move X by: ["); Serial.print(commandValueOne);
+            Serial.print("] at speed: ["); Serial.print(commandValueTwo); Serial.println("]");
             break;
           case 2 :
-            yStepper.move(commandValue);
-            yStepper.setSpeed(500);
+            yStepper.move(commandValueOne);
+            yStepper.setSpeed(commandValueTwo);
             yStepper.runSpeedToPosition();
-            Serial.println("Move Y");
-            break;
-          case 3 :
-            xStepper.setSpeed(commandValue);
-            Serial.println("Set X speed");
-            break;
-          case 4 :
-            yStepper.setSpeed(commandValue);
-            Serial.println("Set Y speed");
+            Serial.print("Move Y by: "); Serial.print(commandValueOne);
+            Serial.print(" at speed: ["); Serial.print(commandValueTwo); Serial.println("]");
             break;
           case 5 :
-            xStepper.setAcceleration(commandValue);
-            Serial.println("Set X acceleration");
-            break;
-          case 6 :
-            yStepper.setAcceleration(commandValue);
-            Serial.println("Set Y acceleration");
+            if (isXDone && isYDone == true) {
+              Serial.println("ok");
+            }
+            else {
+              Serial.println("not_now");
+            }
             break;
           case 11 :
-            xStepper.setCurrentPosition(commandValue);
-            Serial.println("Set X position");
+            xStepper.setCurrentPosition(commandValueOne);
+            Serial.print("Set X position at: "); Serial.println(commandValueOne);
             break;
           case 12 :
-            yStepper.setCurrentPosition(commandValue);
-            Serial.println("Set Y position");
+            yStepper.setCurrentPosition(commandValueOne);
+            Serial.print("Set Y position at: "); Serial.println(commandValueOne);
             break;
           case 13 :
             Serial.println(xStepper.currentPosition());
@@ -147,6 +145,8 @@ void loop() {
             Serial.print("Default case triggered on commandID: ");
             Serial.println(commandID);
         }
+        isXDone = false;
+        isYDone = false;
       }
       command = strtok(0, "&");
     }
@@ -155,10 +155,23 @@ void loop() {
   /*after the "set" part we can now check if we need to move our steppers and then move them
     the accelstepper library suggests that we call run() as often as possible (atleast faster
     than the set speed of the steppers)*/
+
   if (xStepper.distanceToGo() != 0) {
-    xStepper.run();
+    xStepper.runSpeed();
+  }
+  else {
+    isXDone = true;
   }
   if (yStepper.distanceToGo() != 0) {
-    yStepper.run();
+    yStepper.runSpeed();
   }
+  else {
+    isYDone = true;
+  }
+}
+
+void status_update(int commandID, int commandValueOne, int commandValueTwo) {
+  Serial.print("ID= "); Serial.println(commandID);
+  Serial.print("ValueOne= "); Serial.println(commandValueOne);
+  Serial.print("ValueTwo= "); Serial.println(commandValueTwo);
 }
